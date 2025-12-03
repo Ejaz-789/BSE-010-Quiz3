2eae58e74292a8f13c87e8a68c53909e17a6edc8
Ejaz 010 <m.ejazk3211@gmail.com>
2025-12-03 12:49:33 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Java branch!");
+        System.out.println("Hello World from Java!");
+        System.out.println("This is the main branch (Java)");
+        System.out.println("Build executed successfully!");
     }
 }
